ba578a5b4b25722ab0dbd3b386061ffee8f5cafe
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-01 20:47:49 +0100

    RED: New test to check add_entry doesn't work while locked is failing.

--- spec/SecretDiary_spec.rb
+++ spec/SecretDiary_spec.rb
@@ -7,4 +7,9 @@ describe SecretDiary do
     expect((diary.locked?)).to eq(true)
   end
 
+  it "will return error when locked if add_entry is called" do
+    diary = SecretDiary.new
+    expect(diary.add_entry("hello world")).to eq("Error, diary locked.")
+  end
+
 end
