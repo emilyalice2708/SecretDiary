8acdaa8f0dea293e03f88603be2b52f7760fa63f
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-01 20:51:57 +0100

    RED: test checking that get_entries returns errors when locked is failing. Get_entries needs to be created.

--- spec/SecretDiary_spec.rb
+++ spec/SecretDiary_spec.rb
@@ -7,9 +7,14 @@ describe SecretDiary do
     expect((diary.locked?)).to eq(true)
   end
 
-  it "will return error when locked if add_entry is called" do
+  it "will return error message when locked if add_entry is called" do
     diary = SecretDiary.new
     expect(diary.add_entry("hello world")).to eq("Error, diary locked.")
   end
 
+  it "will return error message when locked if get_entries is called" do
+    diary = SecretDiary.new
+    expect(diary.get_entries).to eq("Error, diary locked.")
+  end
+  
 end
